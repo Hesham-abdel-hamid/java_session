37edb709fa27f98af28afedede3def6de4462123
Hesham-abdel-hamid <81419534+Hesham-abdel-hamid@users.noreply.github.com>
2024-03-21 02:10:31 +0200

    21032024

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 window.onload = function () {
   document.write("<h1>LEON</h1>");
   document.querySelector("h1").style.color = "Red";
+  console.error("Be Careful")
+  console.table(["hesham","mohamed","abdelhamid"]);
 };
