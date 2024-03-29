8728330d6f3d095918b3769b5890291528500688
Hesham-abdel-hamid <81419534+Hesham-abdel-hamid@users.noreply.github.com>
2024-03-29 02:43:44 +0200

    29032024

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
+subject = "LEON LEON";
+let content = `
+  <div class="list">
+    <span class="icon">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+    <ul>
+      <li><a class="frist" href="#Services">Services</a></li>
+      <li><a href="#Portfolio">Portfolio</a></li>
+      <li><a href="#About">About</a></li>
+      <li><a class="last" href="#Contacts">Contacts</a></li>
+    </ul>
+  </div>
+  `;
 window.onload = function () {
-  document.write("<h1>LEON</h1>");
+
+  document.write("<h1>", subject, "</h1>");
   document.querySelector("h1").style.color = "Red";
-  console.error("Be Careful")
-  console.table(["hesham","mohamed","abdelhamid"]);
+  console.error(subject);
+  console.table(["hesham", "mohamed", "abdelhamid"]);
+  console.log(typeof "hesham abdelhamid");
+  console.log(typeof 5);
+  console.log(typeof true);
+  console.log(typeof 5.5);
+  console.log(typeof [50, 40, 40]);
+  console.log(typeof { heshma: 50, mohamed: 40, abdelhamid: 40 });
+  console.log(typeof undefined);
+  console.log(typeof null);
+  document.write(content);
+
 };
+
