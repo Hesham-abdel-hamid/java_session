e39bab8f82b55371c45665e39765ffc0375f410f
Hesham-abdel-hamid <81419534+Hesham-abdel-hamid@users.noreply.github.com>
2024-04-17 19:57:48 +0200

    17042024

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+function testFunction() {
+    console.log("Hello");
+    console.log("World");
+    document.write("<h1>", subject, "</h1>");
+    document.write("<h1>", subject, "</h1>");
+    document.write("<h1>", subject, "</h1>");
+    debugger;
+    document.write("<h1>", subject, "</h1>");
+    document.write("<h1>", subject, "</h1>");
+    console.log("my name is hesham abdelhamid".charAt(5));
+}
